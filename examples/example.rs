cdea2e6d64957ae64a714cc107145632d41cad9f
m-mueller678 <marcus.mueller.678@gmail.com>
2025-03-01 14:17:05 +0100

    add example

--- examples/example.rs
+++ examples/example.rs
@@ -1,19 +1,30 @@
-# atomic_pincell
-This is a fork of [atomic_refcell](https://github.com/bholley/atomic_refcell) that adds the ability to pin cell contents like the non-thread-safe [pin-cell](https://github.com/dignifiedquire/pin-cell).
+use atomic_pincell::AtomicRefCell;
+use std::marker::PhantomPinned;
+use std::mem;
+use std::pin::{pin, Pin};
 
-```rust
+#[derive(Default)]
+struct MyPinnedType(u64, PhantomPinned);
+
+fn takes_pinned(_x: Pin<&mut MyPinnedType>) {}
+
+fn takes_ref(x: &MyPinnedType) {
+    println!("{}", x.0);
+}
+
 fn pin_cell() {
     let pinned_cell = pin!(AtomicRefCell::<MyPinnedType, true>::default());
     takes_pinned(pinned_cell.as_ref().borrow_pin_mut().get_pin_mut());
     takes_ref(&pinned_cell.borrow());
 }
-```
 
-This crate also intends to be a drop in replacement for `atomic_refcell`.
-```rust
 fn unpin_cell() {
     let unpinned_cell = AtomicRefCell::<MyPinnedType>::default();
     mem::take(&mut *unpinned_cell.borrow_mut());
     takes_ref(&*unpinned_cell.borrow());
 }
-```
+
+fn main() {
+    unpin_cell();
+    pin_cell();
+}
